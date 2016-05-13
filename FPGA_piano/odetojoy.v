ff1bf5ba49c4936b6060e25cba24d7b3582ac4cb
Cristen Anderson <1cristen1@gmail.com>
2016-05-12 23:06:09 -0700

    Add song state machine for ode to joy

---
+++ FPGA_piano/odetojoy.v
@@ -0,0 +1,106 @@
+`timescale 1ns / 1ps
+`include "piano.v"
+
+module odetojoy (
+	input CLK,
+	input RESET,
+	input [7:0] sw,
+	output reg FREQ,
+	output [7:0] Led
+);
+
+wire [3:0] note;
+
+piano myPiano (
+	.CLK(CLK),
+	.RESET(RESET),
+	.sw(sw),
+	.note(note),
+	.FREQ(FREQ),
+	.Led(Led)
+)
+
+// States
+reg [5:0] state;
+
+always @ (posedge CLK or posedge RESET) begin
+	if (RESET)
+		state <= 6'b0;
+	else begin
+		case (state)
+			6'b000000: state <= (note == E) ? state + 1'b1 : state;
+			6'b000001: state <= (note == none) ? state + 1'b1 : state;
+			6'b000010: state <= (note == E) ? state + 1'b1 : state;
+			6'b000011: state <= (note == none) ? state + 1'b1 : state;
+			6'b000100: state <= (note == F) ? state + 1'b1 : state;
+			6'b000101: state <= (note == none) ? state + 1'b1 : state;
+			6'b000110: state <= (note == G) ? state + 1'b1 : state;
+			6'b000111: state <= (note == none) ? state + 1'b1 : state;
+			6'b001000: state <= (note == G) ? state + 1'b1 : state;
+			6'b001001: state <= (note == none) ? state + 1'b1 : state;
+			6'b001010: state <= (note == F) ? state + 1'b1 : state;
+			6'b001011: state <= (note == none) ? state + 1'b1 : state;
+			6'b001100: state <= (note == E) ? state + 1'b1 : state;
+			6'b001101: state <= (note == none) ? state + 1'b1 : state;
+			6'b001110: state <= (note == D) ? state + 1'b1 : state;
+			6'b001111: state <= (note == none) ? state + 1'b1 : state;
+			6'b010000: state <= (note == C) ? state + 1'b1 : state;
+			6'b010001: state <= (note == none) ? state + 1'b1 : state;
+			6'b010010: state <= (note == C) ? state + 1'b1 : state;
+			6'b010011: state <= (note == none) ? state + 1'b1 : state;
+			6'b010100: state <= (note == D) ? state + 1'b1 : state;
+			6'b010101: state <= (note == none) ? state + 1'b1 : state;
+			6'b010110: state <= (note == E) ? state + 1'b1 : state;
+			6'b010111: state <= (note == none) ? state + 1'b1 : state;
+			6'b011000: state <= (note == E) ? state + 1'b1 : state;
+			6'b011001: state <= (note == none) ? state + 1'b1 : state;
+			6'b011010: state <= (note == D) ? state + 1'b1 : state;
+			6'b011011: state <= (note == none) ? state + 1'b1 : state;
+			6'b011100: state <= (note == D) ? state + 1'b1 : state;
+			6'b011101: state <= (note == none) ? state + 1'b1 : state;
+			6'b011110: state <= 6'b0;
+		endcase
+	end
+end
+
+always @ (posedge CLK or posedge RESET) begin
+	if (RESET)
+		Led <= _E;
+	else begin
+		case (state)
+			6'b000000: Led <= _E;
+			6'b000001: Led <= _E;
+			6'b000010: Led <= _E;
+			6'b000011: Led <= _E;
+			6'b000100: Led <= _F;
+			6'b000101: Led <= _F;
+			6'b000110: Led <= _G;
+			6'b000111: Led <= _G;
+			6'b001000: Led <= _G;
+			6'b001001: Led <= _G;
+			6'b001010: Led <= _F;
+			6'b001011: Led <= _F;
+			6'b001100: Led <= _E;
+			6'b001101: Led <= _E;
+			6'b001110: Led <= _D;
+			6'b001111: Led <= _D;
+			6'b010000: Led <= _C;
+			6'b010001: Led <= _C;
+			6'b010010: Led <= _C;
+			6'b010011: Led <= _C;
+			6'b010100: Led <= _D;
+			6'b010101: Led <= _D;
+			6'b010110: Led <= _E;
+			6'b010111: Led <= _E;
+			6'b011000: Led <= _E;
+			6'b011001: Led <= _E;
+			6'b011010: Led <= _D;
+			6'b011011: Led <= _D;
+			6'b011100: Led <= _D;
+			6'b011101: Led <= _D;
+			6'b011110: Led <= _E;
+		endcase
+	end
+end
+
+endmodule
--- FPGA_piano/piano_tb.v
+++ FPGA_piano/piano_tb.v
@@ -1,20 +1,26 @@
 `timescale 1ns / 1ps
-`include "piano.v"
+`include "odetojoy.v"
+// 7  6 5 4 3 2 1 0
+// C4 D E F G A B C4
 
 module piano_tb;
 
 	// Inputs
 	reg CLK;
 	reg RESET;
+	reg [7:0] sw;
 
 	// Outputs
 	wire FREQ;
+	wire [7:0] Led;
 
 	// Instantiate the Unit Under Test (UUT)
-	piano uut (
+	odetojoy uut (
 		.CLK(CLK), 
 		.RESET(RESET), 
-		.FREQ(FREQ)
+		.sw(sw),
+		.FREQ(FREQ),
+		.Led(Led)
 	);
 
 	initial begin
@@ -23,11 +29,44 @@ module piano_tb;
 		RESET = 1;
 
 		// Wait 100 ns for global reset to finish
-		#100;
-      RESET = 0;
+		#100 RESET = 0;
+
+		#50 sw[5] = 1;
+		#50 sw[5] = 0;
+		#50 sw[5] = 1;
+		#50 sw[5] = 0;
+		#50 sw[4] = 1;
+		#50 sw[4] = 0;
+		#50 sw[3] = 1;
+		#50 sw[3] = 0;
+		#50 sw[3] = 1;
+		#50 sw[3] = 0;
+		#50 sw[4] = 1;
+		#50 sw[4] = 0;
+		#50 sw[5] = 1;
+		#50 sw[5] = 0;
+		#50 sw[6] = 1;
+		#50 sw[6] = 0;
+		#50 sw[7] = 1;
+		#50 sw[7] = 0;
+		#50 sw[7] = 1;
+		#50 sw[7] = 0;
+		#50 sw[6] = 1;
+		#50 sw[6] = 0;
+		#50 sw[5] = 1;
+		#50 sw[5] = 0;
+		#50 sw[5] = 1;
+		#50 sw[5] = 0;
+		#50 sw[4] = 1;
+		#50 sw[4] = 0;
+		#50 sw[4] = 1;
+		#50 sw[4] = 0;
 	end
 
   always #1 CLK = ~CLK;
+
+  initial
+		#2000 $finish;
       
 endmodule
 
