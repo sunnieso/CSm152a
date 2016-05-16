a3b51425cc3d3cf838e9e3048a35ef8f1b7e9078
Cristen Anderson <1cristen1@gmail.com>
2016-05-16 15:49:33 -0700

    fix display output

--- FPGA_piano/display.v
+++ FPGA_piano/display.v
@@ -8,16 +8,16 @@ module segDisplay(
 
 	// ASYNCHRONOUS assign 7 segment display based on the current note
 	always @ (note) begin
-		case(note)       //PGFEDCBA
+		case(note)
 			4'h0: seg = 8'b11000110; // C
-			4'h1: seg = 8'b11000000; // D
-			4'h2: seg = 8'b10000110; // E
-			4'h3: seg = 8'b10001110; // F
-			4'h4: seg = 8'b10000010; // G
-			4'h5: seg = 8'b10001000; // A
-			4'h6: seg = 8'b10000000; // B
+			4'h1: seg = 8'b10000000; // B
+			4'h2: seg = 8'b10001000; // A
+			4'h3: seg = 8'b10000010; // G
+			4'h4: seg = 8'b10001110; // F
+			4'h5: seg = 8'b10000110; // E
+			4'h6: seg = 8'b11000000; // D
 			4'h7: seg = 8'b01000110; // C
-			default: seg = 8'b11111111; // nono
+			default: seg = 8'b11111111; // none
 		endcase
 		an = 4'b1110;
 	end
