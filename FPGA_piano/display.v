e8509612466aaf493125ffb8a09dc8c2ecbdcea5
1842_Sunnie <sunnieso3741@gmail.com>
2016-05-16 14:45:55 -0700

    fix bugs, test on board

--- FPGA_piano/display.v
+++ FPGA_piano/display.v
@@ -12,9 +12,7 @@ parameter E = 4'b0101;
 parameter D = 4'b0110;
 parameter C4 = 4'b0111;
 parameter none = 4'b1000;
-
 Seven segment display:
-
  AAA
 F   B
  GGG
@@ -24,24 +22,22 @@ E   C
 */
 module segDisplay(
 	input [3:0] note,
-	output [7:0] seg,
-	output [3:0] an
+	output reg [7:0] seg,
+	output reg [3:0] an
 	);
 
 	// ASYNCHRONOUS assign 7 segment display based on the current note
 	always @ (note) begin
 		case(note)       //PGFEDCBA
 			4'h0: seg = 8'b11000110; // C
-			4'h1: seg = 8'b11000000; //	D
+			4'h1: seg = 8'b11000000; // D
 			4'h2: seg = 8'b10000110; // E
 			4'h3: seg = 8'b10001110; // F
 			4'h4: seg = 8'b10000010; // G
 			4'h5: seg = 8'b10001000; // A
 			4'h6: seg = 8'b10000000; // B
-			4'h7: seg = 8'b01000110;
-			4'h8: seg = 8'b01000000;
-			4'h9: seg = 8'b00000110;
-			default: seg = 8'b11111111;
+			4'h7: seg = 8'b01000110; // C
+			default: seg = 8'b11111111; // nono
 		endcase
 		an = 4'b1110;
 	end
