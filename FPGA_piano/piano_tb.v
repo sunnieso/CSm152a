60bf89cb62f4570ab6774d4fe3e375b00663ffb9
Sunnie So <sunnieso3741@ucla.edu>
2016-05-31 00:30:51 -0700

    change testbench

--- FPGA_piano/piano_tb.v
+++ FPGA_piano/piano_tb.v
@@ -1,103 +1,287 @@
 `timescale 1ns / 1ps
-`include "piano.v"
-`include "freq.v"
-`include "notes.v"
-`include "odetojoy.v"
-`include "display.v"
-`include "debounce.v"
-`include "doReMiAUTO.v"
-`include "doReMi.v"
-// 7  6 5 4 3 2 1 0
-// C4 D E F G A B C4
 
 module piano_tb;
 
 	// Inputs
 	reg CLK;
 	reg RESET;
+	reg ODETOJOY;
+	reg ODETOJOY_AUTO;
+	reg DOREMI;
+	reg DOREMI_AUTO;
 	reg [7:0] sw;
-	reg MODE;
-	reg MODE2;
-	
+
 	// Outputs
 	wire FREQ;
 	wire [7:0] Led;
 	wire [7:0] seg;
 	wire [3:0] an;
 
-
 	// Instantiate the Unit Under Test (UUT)
 	piano uut (
 		.CLK(CLK), 
 		.RESET(RESET), 
-		.MODE(MODE),
-		.MODE2(MODE2),
-		.sw(sw),
-		.FREQ(FREQ),
-		.Led(Led),
-		.seg(seg),
+		.ODETOJOY(ODETOJOY), 
+		.ODETOJOY_AUTO(ODETOJOY_AUTO), 
+		.DOREMI(DOREMI), 
+		.DOREMI_AUTO(DOREMI_AUTO), 
+		.sw(sw), 
+		.FREQ(FREQ), 
+		.Led(Led), 
+		.seg(seg), 
 		.an(an)
 	);
 
 	initial begin
 		// Initialize Inputs
-		CLK = 1;
-		RESET = 1;
-		sw = 8'b0;
-		MODE = 0;
-		MODE2 = 0;
-		
+		CLK = 0;
+		RESET = 0;
+		ODETOJOY = 0;
+		ODETOJOY_AUTO = 0;
+		DOREMI = 0;
+		DOREMI_AUTO = 0;
+		sw = 0;
+
 		// Wait 100 ns for global reset to finish
-		#100 RESET = 0;
-		#1 MODE = 1;
-		#1 MODE = 0;
-		#1 MODE = 1;
-		#1 MODE = 0;
-		#1 MODE = 1;
-		#1 MODE = 0;
-		#1 MODE = 1;
-		#1 MODE = 0;
-		#10 MODE = 1;
-		#10 MODE = 0;
-		#50 sw[5] = 1;
-		#50 sw[5] = 0;
-		#50 sw[5] = 1;
-		#50 sw[5] = 0;
-		#50 sw[4] = 1;
-		#50 sw[4] = 0;
-		#50 sw[3] = 1;
-		#50 sw[3] = 0;
-		#50 sw[3] = 1;
-		#50 sw[3] = 0;
-		#50 sw[4] = 1;
-		#50 sw[4] = 0;
-		#50 sw[5] = 1;
-		#50 sw[5] = 0;
-		#50 sw[6] = 1;
-		#50 sw[6] = 0;
-		#50 sw[7] = 1;
-		#50 sw[7] = 0;
-		#50 sw[7] = 1;
-		#50 sw[7] = 0;
-		#50 sw[6] = 1;
-		#50 sw[6] = 0;
-		#50 sw[5] = 1;
-		#50 sw[5] = 0;
-		#50 sw[5] = 1;
-		#50 sw[5] = 0;
-		#50 sw[4] = 1;
-		#50 sw[4] = 0;
-		#50 sw[4] = 1;
-		#50 sw[4] = 0;
+		#100;
+        
+		// Add stimulus here
+		#1 ODETOJOY_AUTO = 1;
+		#1 ODETOJOY_AUTO = 0;
+		#1 ODETOJOY_AUTO = 1;
+		#1 ODETOJOY_AUTO = 0;
+		#1 ODETOJOY_AUTO = 1;
+
+		#100 ODETOJOY_AUTO = 0;
+
+
+		#1 DOREMI_AUTO = 1;
+		#1 DOREMI_AUTO = 0;
+		#1 DOREMI_AUTO = 1;
+		#1 DOREMI_AUTO = 0;
+		#1 DOREMI_AUTO = 1;
+
+		#100 DOREMI_AUTO = 0;
+
+
+
+		#1 ODETOJOY = 1;
+		#1 ODETOJOY = 0;
+		#1 ODETOJOY = 1;
+		#1 ODETOJOY = 0;
+		#1 ODETOJOY = 1;
+
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0; 
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[4] = 1; //d
+			#50 sw[4] = 0;
+			#50 sw[4] = 1; //d
+			#50 sw[4] = 0;
+
+
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0; 
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0;
+
+		#10 ODETOJOY = 0;
+
+
+		#1 DOREMI = 1;
+		#1 DOREMI = 0;
+		#1 DOREMI = 1;
+		#1 DOREMI = 0;
+		#1 DOREMI = 1;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0; 
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0; 
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0; 
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[7] = 1; //c
+			#50 sw[7] = 0; 
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0; 
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+			#50 sw[1] = 1; //b
+			#50 sw[1] = 0;
+
+
+			#50 sw[1] = 1; //b
+			#50 sw[1] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0; 
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+			#50 sw[1] = 1; //b
+			#50 sw[1] = 0;
+			#50 sw[0] = 1; //c
+			#50 sw[0] = 0;
+
+
+			#50 sw[0] = 1; //c
+			#50 sw[0] = 0;
+			#50 sw[1] = 1; //b
+			#50 sw[1] = 0;
+			#50 sw[2] = 1; //a
+			#50 sw[2] = 0;
+			#50 sw[4] = 1; //f
+			#50 sw[4] = 0; 
+			#50 sw[1] = 1; //b
+			#50 sw[1] = 0;
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[0] = 1; //c
+			#50 sw[0] = 0;
+			
+			#50 sw[3] = 1; //g
+			#50 sw[3] = 0;
+			#50 sw[5] = 1; //e
+			#50 sw[5] = 0;
+			#50 sw[6] = 1; //d
+			#50 sw[6] = 0;
+		#100 DOREMI = 0;
+
+
 	end
 
-  always #1 CLK = ~CLK;
+  	always #1 CLK = ~CLK;
 
- // always #5 mode = ~mode;
+ 	// always #5 mode = ~mode;
 
 
-  initial
-		#2000 $finish;
+  	initial
+		#20000 $finish;
       
 endmodule
 
